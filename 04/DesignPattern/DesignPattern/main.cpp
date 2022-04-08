d452fdd2f69c5e1527ebfe5cdcd2c76862368a6d
delims <delims@qq.com>
2022-04-08 19:18:55 +0800

    update

--- 04/DesignPattern/DesignPattern/main.cpp
+++ 04/DesignPattern/DesignPattern/main.cpp
@@ -10,13 +10,15 @@
 extern void test_FactoryPattern();
 extern void test_AbstractFactoryPattern();
 extern void test_Singleton();
-extern void test_DesignPattern();
 extern void test_PrototypePattern();
 extern void test_BridgePattern();
+extern void test_StatusPattern();
+extern void test_StrategyPattern();
+extern void test_TemplatePattern();
 
 int main(int argc, const char * argv[]) {
         
-    test_BridgePattern();
+    test_TemplatePattern();
     
     return 0;
 }
