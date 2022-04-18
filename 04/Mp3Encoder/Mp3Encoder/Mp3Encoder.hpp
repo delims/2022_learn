fb32547af38469d86bf19af8306683e2f83c2754
delims <delims@qq.com>
2022-04-18 18:47:50 +0800

    update

--- 04/Mp3Encoder/Mp3Encoder/Mp3Encoder.hpp
+++ 04/Mp3Encoder/Mp3Encoder/Mp3Encoder.hpp
@@ -9,10 +9,17 @@
 #define Mp3Encoder_hpp
 
 #include <stdio.h>
+#include "lame.h"
 
 class Mp3Encoder {
+private:
+    FILE* pcmFile;
+    FILE* mp3File;
+    lame_t lameClient;
 public:
+    int init(const char* pcmFilePath, const char *mp3FilePath, int sampleRate, int channels, int bitRate);
     void encode();
+    void destory();
 };
 
 
--- 04/Mp3Encoder/Mp3Encoder/ViewController.mm
+++ 04/Mp3Encoder/Mp3Encoder/ViewController.mm
@@ -21,8 +21,27 @@
     
     self.view.backgroundColor = UIColor.whiteColor;
     
+    NSString *pcmFilePath = [NSBundle.mainBundle pathForResource:@"冰雨片段8k16bit单声道" ofType:@"pcm"];
+    
+    bool exist =  [NSFileManager.defaultManager fileExistsAtPath:pcmFilePath];
+    
+    NSArray *contents =  [NSFileManager.defaultManager contentsOfDirectoryAtPath:NSBundle.mainBundle.bundlePath error:nil ];
+    
+//    for (NSString *item in contents) {
+//        NSLog(item);
+//    }
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *mp3Path = [documentsDirectory stringByAppendingPathComponent:@"vocal.mp3"];
+    
     Mp3Encoder *mp3 = new Mp3Encoder();
+
+    mp3->init(pcmFilePath.UTF8String, mp3Path.UTF8String, 4000, 1, 3000);
     mp3->encode();
+
+    NSLog(@"%d",exist);
+    
     delete mp3;
     
 }
